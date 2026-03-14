7c90f64c1597e02c4e4dd39479bc0a6deb5e2b98
Artem Petrov <partos0511@gmail.com>
2026-03-14 23:24:10 +0400

    fix migration

--- db/migrate/20260306100000_add_stdio_support_to_mcp_servers.rb
+++ db/migrate/20260306100000_add_stdio_support_to_mcp_servers.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddStdioSupportToMcpServers < ActiveRecord::Migration[7.2]
+class AddStdioSupportToMCPServers < ActiveRecord::Migration[7.2]
   def change
     add_column :mcp_servers, :command, :string
   end
