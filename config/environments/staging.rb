f888fb4ab4941a2813a48ddf0f79f50d2a737a6d
Rozhnov Alexandr <gnox73@gmail.com>
2026-03-25 23:45:51 +0400

    Increase time to start an agent container due to kube node allocation.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,5 +2,4 @@ require_relative "production"
 
 Rails.application.configure do
   # Staging currently follows production defaults unless explicitly overridden here.
-  config.log_level = :debug
 end
