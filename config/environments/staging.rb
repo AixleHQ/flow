1504914c22ec884d8115f743dd63b08c7d4e76ff
Rozhnov Alexandr <gnox73@gmail.com>
2026-03-25 17:21:17 +0400

    Set debug level for staging env.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,4 +2,5 @@ require_relative "production"
 
 Rails.application.configure do
   # Staging currently follows production defaults unless explicitly overridden here.
+  config.log_level = :debug
 end
